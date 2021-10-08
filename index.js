05142c3953f6133ac28b7644bbf480e0172937cf
ruka-Design <2002rukahosomi@gmail.com>
2021-10-08 23:54:18 +0900

    大幅な書き直し

--- index.js
+++ index.js
@@ -2,34 +2,22 @@
 //「もし○○ならば●●を行う」の処理のこと
 //ifの後ろに条件式を書き、それが「成り立つ」場合の処理を{ }の中に書きます。
 
-const text = document.getElementById("text");
+let value = document.getElementById("text").value;
+console.log(value);
 
-document.getElementById("btn").onclick = function () {
-  let value = text.value;
-  // document.getElementById("box__red").style.display = "none";
-  if (value == "赤") {
-    document.getElementById("box__red").style.display = "block";
-  }
+const btn = document.getElementById("btn");
 
-  if (value == "黄色") {
-    document.getElementById("box__yellow").style.display = "block";
-  }
-
-  if (value == "青") {
-    document.getElementById("box__blu").style.display = "block";
-  }
-
-  console.log(value);
+btn.onclick = function () {
+  console.log(btn);
 };
+const colors = [
+  { name: "赤", colorCode: "red" },
+  { name: "青", colorCode: "bule" },
+  { name: "緑", colorCode: "green" },
+];
 
-const level = 12;
-const a = 14;
+const color = colors.find((value) => {
+  return value.name === "赤";
+});
 
-// 条件式を「level > 10」とするif文を作ってください
-if (level > 10) {
-  console.log("レベルが10より大きいです");
-}
-
-if (a == 14) {
-  console.log("同じです！");
-}
+console.log(color);
