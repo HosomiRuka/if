f84550dbe4bb04a56aa5f80637df77acbcd1b308
ruka-Design <2002rukahosomi@gmail.com>
2021-10-09 01:11:08 +0900

    色の指定をカラッピーカにしました

--- index.js
+++ index.js
@@ -2,22 +2,22 @@
 //「もし○○ならば●●を行う」の処理のこと
 //ifの後ろに条件式を書き、それが「成り立つ」場合の処理を{ }の中に書きます。
 
-let value = document.getElementById("text").value;
-console.log(value);
-
+let text = document.getElementById("text");
+let textBox = document.getElementById("textBox");
+let color = document.getElementById("color");
 const btn = document.getElementById("btn");
+const box = document.getElementById("box");
 
 btn.onclick = function () {
-  console.log(btn);
+  // console.log(btn);
+  let value = text.value;
+  let colorValue = color.value;
+
+  console.log(value);
+  console.log(colorValue);
+  console.log(textBox.innerHTML);
+
+  // console.log(color);
+  box.style.backgroundColor = colorValue;
+  textBox.innerHTML = text.value;
 };
-const colors = [
-  { name: "赤", colorCode: "red" },
-  { name: "青", colorCode: "bule" },
-  { name: "緑", colorCode: "green" },
-];
-
-const color = colors.find((value) => {
-  return value.name === "赤";
-});
-
-console.log(color);
