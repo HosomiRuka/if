483dd095965fa3a043d331f8549d579b1e94548c
ruka-Design <2002rukahosomi@gmail.com>
2021-10-09 02:06:34 +0900

    コメントを追加しました

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 //if文とは？
 //「もし○○ならば●●を行う」の処理のこと
 //ifの後ろに条件式を書き、それが「成り立つ」場合の処理を{ }の中に書きます。
-
 let text = document.getElementById("text");
 let textBox = document.getElementById("textBox");
 let color = document.getElementById("color");
