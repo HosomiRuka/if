7889dcb955d8ddaf6198dc9a90737cfef54e79fb
ruka-Design <2002rukahosomi@gmail.com>
2021-10-11 00:54:39 +0900

    コメントを追加しました

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ let text = document.getElementById("text"); //#textを取得してきて変数te
 let textBox = document.getElementById("textBox"); //#textBoxを取得してきて変数testBoxに代入しています
 let box = document.getElementById("box"); //#boxを取得してきて変数boxに代入しています
 let parent = document.getElementById("menueText"); //#menueTextを取得してきて変数parentに代入しています
-let area = document.createElement("h2"); //createElement()でjs内でh1を生成する動作を、変数areaに代入しています
+let area = document.createElement("h2"); //createElement()でjs内でh2を生成する動作を、変数areaに代入しています
 const btn = document.getElementById("btn"); //#btnを取得してきて変数btnに代入しています
 const deleteBtn = document.getElementById("deleteBtn"); //#deleteBtnを取得してきて変数deleteBtnに代入しています、delete単体は予約後なので使え無かったです。
 
@@ -18,12 +18,12 @@ btn.onclick = function () {
   let textValue = text.value; //変数valueに#textのvalue値を代入しています
   let colorValeu = color.value; //変数colorValueに#colorのvalue値を代入しています
 
-  area.innerHTML = "createElementとappendChildで生成されたh1です";
-  parent.appendChild(area);
+  area.innerHTML = "createElementとappendChildで生成されたh2です"; //js内で生成したh2の中にテキストを入れています
+  parent.appendChild(area); //js内で生成したh2をparent(#menueText)の末尾に追加します
 
-  box.style.backgroundColor = colorValeu;
-  box.style.display = "block";
-  textBox.innerHTML = textValue;
+  box.style.backgroundColor = colorValeu; //#boxの背景色にcolorで設定した色を代入しています
+  box.style.display = "block"; //noneで隠している#boxを表示させます
+  textBox.innerHTML = textValue; //#textBoxに#textで入力した値を代入しています
 
   // console.log(value);
   // console.log(colorValue);
@@ -31,6 +31,8 @@ btn.onclick = function () {
   // console.log(area);
 };
 
+//#deleteBtnを押したとき
 deleteBtn.onclick = function () {
+  //#boxのdisplayの値がnoneになり消えます
   box.style.display = "none";
 };
